ec321a5a7521a5d905eaebd97b254fd897ea9e5e
caio2983 <caioluna2002@gmail.com>
2025-07-16 19:06:02 -0300

    fix: move app listen line

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
   const config = new DocumentBuilder()
     .setTitle('Todo API')
     .setDescription('Todo API Docs')
@@ -13,5 +13,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
