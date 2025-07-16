43c09a39cfad592e551a119faee867eb5e7dc575
caio2983 <caioluna2002@gmail.com>
2025-07-16 19:29:58 -0300

    feat: update orm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskModule } from './task/task.module';
+import { Task } from './task/entities/task.entity';
+
 @Module({
-  imports: [TaskModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'password',
+      database: 'TodoNestNext',
+      entities: [Task],
+      synchronize: true,
+      logging: true,
+    }),
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
