a5f2fb641cf4d4140107756708065e53e15a5caf
caio2983 <caioluna2002@gmail.com>
2025-07-16 19:09:28 -0300

    feat: update task dto

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class CreateTaskDto {
+  @IsNotEmpty()
+  @MinLength(1)
+  @IsString()
   @ApiProperty({ example: 'Study' })
   title: string;
 
+  @IsBoolean()
   @ApiProperty({ example: false })
   done: boolean;
 }
